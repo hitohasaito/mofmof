dd804c55759af25032e6c064e293c3e2607b8339
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-07-11 20:08:09 +0900

    【add】デバック用のgemをインストール,投稿機能を作成

--- app/controllers/estates_controller.rb
+++ app/controllers/estates_controller.rb
@@ -2,7 +2,26 @@ class EstatesController < ApplicationController
   def index
   end
   def new
-    @estate = Estate.new
-    @station = Station.new
+    @estates = Estate.new
+    @stations = Station.new
+  end
+  def create
+    @estates = Estate.new(estate_params)
+    @stations = Station.new(station_params)
+    if @estates.save && @stations.save
+      redirect_to estates_path
+    else
+      render "new"
+    end
+  end
+
+
+  private
+
+  def estate_params
+    params.require(:estate).permit(:name,:fee,:adress,:age,:note)
+  end
+  def station_params
+    params.require(:station).permit(:station1,:line1,:walking_minutes1,:station2,:line2,:walking_minutes2)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :estates
-  resources :stations
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :estates do
+    resources :stations
+  end
 end
